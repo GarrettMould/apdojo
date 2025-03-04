bd42c038019a8b7b7cfdc85255d578893bdd2542
Garrett Mould <garrettmould@gmail.com>
2025-03-04 14:21:39 +0700

    Add structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,12 @@ import ExamDayCountdown from '@/components/ExamDayCountdown'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to APDojo</h1>
+    <main>
+      <Hero />
+      <LiveTestMockup />
+      <ExamOptionsPreview />
+      <ExamDayCountdown />
+      <VideoLibraryPreview />
     </main>
   )
 }
