d6171775ade582f057e0011fad7f90c2f72e19b3
Garrett Mould <garrettmould@gmail.com>
2025-03-30 14:12:35 +0700

    removed AI Dojo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@
 import { Hero } from "@/components/hero"
 import LiveTestMockup from '@/components/previews/liveTestMockup'
 import VideoLibraryPreview from "@/components/previews/VidLibraryPreview"
-import { QuestionsGrid } from "@/components/questionsGrid"
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <VideoLibraryPreview />
-      <QuestionsGrid></QuestionsGrid>
       <LiveTestMockup />
     </main>
   )
